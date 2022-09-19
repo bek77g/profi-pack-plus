954654cbfae05ed0132c6bca6bc233dba1e6760e
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-09-19 13:24:38 +0600

    feat: created fake data

---
+++ src/components/constants/index.js
@@ -0,0 +1,28 @@
+const search = [
+    {
+        name: "Хозяйственные товары",
+        img: "https://profipackpluskgz.1c-umi.ru/images/cms/thumbs/a5b0aeaa3fa7d6e58d75710c18673bd7ec6d5f6d/6087812252_220_220.jpg",
+    },
+    {
+        name: "Обёрточная бумага",
+        img: "https://profipackpluskgz.1c-umi.ru/images/cms/thumbs/a5b0aeaa3fa7d6e58d75710c18673bd7ec6d5f6d/6087812252_220_220.jpg",
+    },
+    {
+        name: "Перчатки",
+        img: "https://profipackpluskgz.1c-umi.ru/images/cms/thumbs/a5b0aeaa3fa7d6e58d75710c18673bd7ec6d5f6d/6087812252_220_220.jpg",
+    },
+    {
+        name: "Пакеты дой-пак",
+        img: "https://profipackpluskgz.1c-umi.ru/images/cms/thumbs/a5b0aeaa3fa7d6e58d75710c18673bd7ec6d5f6d/6087812252_220_220.jpg",
+    },
+    {
+        name: "Эко контейнера",
+        img: "https://profipackpluskgz.1c-umi.ru/images/cms/thumbs/a5b0aeaa3fa7d6e58d75710c18673bd7ec6d5f6d/6087812252_220_220.jpg",
+    },
+    {
+        name: "Бумажные стаканы",
+        img: "https://profipackpluskgz.1c-umi.ru/images/cms/thumbs/a5b0aeaa3fa7d6e58d75710c18673bd7ec6d5f6d/6087812252_220_220.jpg",
+    },
+]
+
+export default search;
