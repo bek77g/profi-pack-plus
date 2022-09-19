357e394b9e938ad48c04dc87f725632da03b36c4
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-09-19 13:15:09 +0600

    feat: imported advertise component

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPageAdvertise from "./components/MainPageAdvertise/MainPageAdvertise";
+
+const MainPage = () => {
+    return (
+        <div className="mainPage">
+            <MainPageAdvertise/>
+        </div>
+    );
+};
+
+export default MainPage;
