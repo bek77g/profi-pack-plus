cddf1bfaac4109fae92c50e14a269b91e7643e97
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-09-19 13:14:37 +0600

    feat: added slider for advertise component

---
+++ src/pages/MainPage/components/MainPageAdvertise/MainPageAdvertiseSlider.js
@@ -0,0 +1,33 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+const MainPageAdvertiseSlider = () => {
+    return (
+        <div className="mainPageAdvertiseSlider">
+            <Carousel variant="dark">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://md-eksperiment.org/images/posts/0f60e764-5372-40e6-bb78-19d41fe2b1be.jpeg"
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://md-eksperiment.org/images/posts/0f60e764-5372-40e6-bb78-19d41fe2b1be.jpeg"
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://md-eksperiment.org/images/posts/0f60e764-5372-40e6-bb78-19d41fe2b1be.jpeg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+}
+
+export default MainPageAdvertiseSlider;
