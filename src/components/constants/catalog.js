03f2880c065001e494f59f3df6cdb50e413a9abf
Tokonov Tagai <tokonov.tagai@gmail.com>
2022-09-20 10:29:06 +0600

    feat: added fake data for catalog cards

---
+++ src/components/constants/catalog.js
@@ -0,0 +1,36 @@
+const catalog = [
+    {
+        name: "Хозяйственные товары",
+        img: "https://md-eksperiment.org/images/posts/0f60e764-5372-40e6-bb78-19d41fe2b1be.jpeg",
+        price: "9800 сом",
+        description: "Водонепроницаемый Рюкзак"
+    },
+    {
+        name: "Хозяйственные товары",
+        img: "https://md-eksperiment.org/images/posts/0f60e764-5372-40e6-bb78-19d41fe2b1be.jpeg",
+        price: "6 900 сом",
+        description: "Спасательный жилет BRP Men's Airflow PFD"
+    },
+    {
+        name: "Хозяйственные товары",
+        img: "https://md-eksperiment.org/images/posts/0f60e764-5372-40e6-bb78-19d41fe2b1be.jpeg",
+        price: "5000 сом",
+        description:
+            "BRP Audio-Premium System"
+    },
+    {
+        name: "Хозяйственные товары",
+        img: "https://md-eksperiment.org/images/posts/0f60e764-5372-40e6-bb78-19d41fe2b1be.jpeg",
+        price: "35000 сом",
+        description: "Водонепроницаемый Рюкзак"
+    },
+    // {
+    //     name: "Хозяйственные товары",
+    //     img: "https://md-eksperiment.org/images/posts/0f60e764-5372-40e6-bb78-19d41fe2b1be.jpeg",
+    //     price: "1000 сом",
+    //     description: "Водонепроницаемый Рюкзак"
+    // },
+
+]
+
+export default catalog;
