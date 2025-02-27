685e18657b55841a987fb85c4f8e23bbd8b326d9
bek77g <d.zheenkul@gmail.com>
2025-02-28 02:30:55 +0600

    fix: breadcrumbs in profile page

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,45 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import arr from '../../assets/icons/arr.svg';
+import SEO from '../../hoc/SEO';
 import AddressInfo from './components/AddressInfo';
 import OrganizationInfo from './components/OrganizationInfo';
 import PersonalInfo from './components/PersonalInfo';
-import SEO from '../../hoc/SEO';
-import arr from '../../assets/icons/arr.svg';
 import './ProfilePage.scss';
 
 const ProfilePage = () => {
-    return (
-        <>
-            <SEO
-                SeoTitle="ProfiPackPlus - Личный кабинет"
-                SeoDescription="Управление личными данными в магазине Profipackplus"
-            />
-            <div className="profilePage">
-                <div className="breadcrumbs">
-                    <Link to="/">
-                        Главная 
-                    </Link> / <span>Личный кабинет</span>
-                </div>
+	return (
+		<>
+			<SEO
+				SeoTitle='ProfiPackPlus - Личный кабинет'
+				SeoDescription='Управление личными данными в магазине Profipackplus'
+			/>
+			<div className='profilePage'>
+				<div className='profilePage__top'>
+					<span>
+						<Link to='/'>
+							Главная <img src={arr} alt='arr' />
+						</Link>
+					</span>
+					<span>Личный кабинет</span>
+				</div>
 
-                <div className="profilePage__sections">
-                    <div className="profilePage__section">
-                        <h3>Личная информация</h3>
-                        <PersonalInfo />
-                    </div>
+				<div className='profilePage__sections'>
+					<div className='profilePage__section'>
+						<h3>Личная информация</h3>
+						<PersonalInfo />
+					</div>
 
-                    <div className="profilePage__section">
-                        <h3>Информация об организации</h3>
-                        <OrganizationInfo />
-                    </div>
+					<div className='profilePage__section'>
+						<h3>Информация об организации</h3>
+						<OrganizationInfo />
+					</div>
 
-                    <div className="profilePage__section">
-                        <h3>Адреса доставки</h3>
-                        <AddressInfo />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+					<div className='profilePage__section'>
+						<h3>Адреса доставки</h3>
+						<AddressInfo />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ProfilePage;
