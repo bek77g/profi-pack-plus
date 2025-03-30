b6279acfbe5e774f2055daf819a7c75c4d705cc2
bek77g <d.zheenkul@gmail.com>
2025-03-30 12:36:40 +0600

    fix: not logged profile view

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import arr from '../../assets/icons/arr.svg';
+import { CustomContext } from '../../hoc/mainContentContext';
 import SEO from '../../hoc/SEO';
 import AddressInfo from './components/AddressInfo';
 import OrganizationInfo from './components/OrganizationInfo';
@@ -8,6 +9,12 @@ import PersonalInfo from './components/PersonalInfo';
 import './ProfilePage.scss';
 
 const ProfilePage = () => {
+	const { logout, user, setAuthModalOpen } = useContext(CustomContext);
+
+	const handleLogin = () => {
+		setAuthModalOpen(true);
+	};
+
 	return (
 		<>
 			<SEO
@@ -24,22 +31,43 @@ const ProfilePage = () => {
 					<span>Личный кабинет</span>
 				</div>
 
-				<div className='profilePage__sections'>
-					<div className='profilePage__section'>
-						<h3>Личная информация</h3>
-						<PersonalInfo />
-					</div>
+				{user ? (
+					<div className='profilePage__sections'>
+						<div className='profilePage__section'>
+							<h3>Личная информация</h3>
+							<PersonalInfo />
+						</div>
 
-					<div className='profilePage__section'>
-						<h3>Информация об организации</h3>
-						<OrganizationInfo />
-					</div>
+						<div className='profilePage__section'>
+							<h3>Информация об организации</h3>
+							<OrganizationInfo />
+						</div>
 
-					<div className='profilePage__section'>
-						<h3>Адреса доставки</h3>
-						<AddressInfo />
+						<div className='profilePage__section'>
+							<h3>Адреса доставки</h3>
+							<AddressInfo />
+						</div>
+
+						<div className='profilePage__section profilePage__logout'>
+							<button className='btn btn-danger' onClick={logout}>
+								ВЫЙТИ
+							</button>
+						</div>
 					</div>
-				</div>
+				) : (
+					<div className='profilePage__not-logged-in'>
+						<div className='profilePage__section'>
+							<h3>Вы не авторизованы</h3>
+							<p>Для доступа к личному кабинету необходимо войти в систему.</p>
+							<button 
+								className='btn btn-primary profilePage__login-btn' 
+								onClick={handleLogin}
+							>
+								ВОЙТИ В АККАУНТ
+							</button>
+						</div>
+					</div>
+				)}
 			</div>
 		</>
 	);
